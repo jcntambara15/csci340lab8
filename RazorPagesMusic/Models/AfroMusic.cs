14dd1eff0704c7754edbd6005467589ba1e29a5f
Jean Claude Ntambara <jc.ntambara@gmail.com>
2023-10-15 20:47:54 -0500

    search and validation

--- RazorPagesMusic/Models/AfroMusic.cs
+++ RazorPagesMusic/Models/AfroMusic.cs
@@ -1,14 +1,31 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMusic.Models;
 
 public class Music
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(40)]
+    [Required]
     public string? Artist { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
     public string? Genre { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [StringLength(30)]
     public string? Country { get; set; }
+
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
 }
--- RazorPagesMusic/Pages/AfroMusic/Index.cshtml.cs
+++ RazorPagesMusic/Pages/AfroMusic/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMusic.Models;
 using RazorPagesMusic.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RazorPagesMusic.Pages.AfroMusic
 {
@@ -21,12 +22,35 @@ namespace RazorPagesMusic.Pages.AfroMusic
 
         public IList<Music> AfroMusic { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Titles { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MusicTitle { get; set; }
+
         public async Task OnGetAsync()
         {
-            if (_context.AfroMusic != null)
+            // Use LINQ to get list of genres.
+            IQueryable<string> titleQuery = from m in _context.AfroMusic
+                                            orderby m.Artist
+                                            select m.Artist;
+
+            var musics = from m in _context.AfroMusic
+                        select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                AfroMusic = await _context.AfroMusic.ToListAsync();
+                musics = musics.Where(s => s.Title.Contains(SearchString));
             }
+
+            if (!string.IsNullOrEmpty(MusicTitle))
+            {
+                musics = musics.Where(x => x.Artist == MusicTitle);
+            }
+            Titles = new SelectList(await titleQuery.Distinct().ToListAsync());
+            AfroMusic = await musics.ToListAsync();
         }
     }
 }
